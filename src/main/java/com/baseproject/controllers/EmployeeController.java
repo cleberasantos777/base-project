bfd0d5d7a7b76bc37134c69445095c7e52867c34
Cleber <fiscal@Embragen.local>
2021-08-20 14:03:03 -0300

    bp 20082021_1402

--- src/main/java/com/baseproject/controllers/EmployeeController.java
+++ src/main/java/com/baseproject/controllers/EmployeeController.java
@@ -1,9 +1,12 @@
 package com.baseproject.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.baseproject.entities.EmployeeEntity;
 import com.baseproject.repositories.EmployeeRepository;
 
 @RestController
@@ -13,10 +16,14 @@ public class EmployeeController {
 	@Autowired
 	EmployeeRepository er;
 	
-	/*
-	@PostMapping
-	public ResponseEntity<EmployeeEntity> createNewEmployee(){
-		
+	@PostMapping(value = "/registration")
+	public String createEmployee(EmployeeEntity employeeEntity) {
+		er.save(employeeEntity);
+		return "redirect/registration";		
 	}
-	*/
+	
+	@GetMapping(value = "/teste")
+	public String teste() {
+		return "teste";
+	}
 }
